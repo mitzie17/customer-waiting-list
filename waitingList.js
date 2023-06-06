e04dafcbb45244aabc98f1ad8551df401f1a51bf
Mitzi17 <m_estrada17@live.com>
2023-06-05 17:19:00 -0700

    added event listener to button

---
+++ waitingList.js
@@ -0,0 +1,8 @@
+let id = 0;
+
+document.getElementById('add').addEventListener('click', () => {
+    let table = document.getElementById('list');
+    let row = table.insertRow(1);
+    row.setAttribute('id', `item-${id}`);
+    row.insertCell(0).innerHTML = document.getElementById('customer-name').value
+})
