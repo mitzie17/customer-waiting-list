1b0342393618343f41be3b1d5b3ef2e381f01bf1
Mitzi17 <m_estrada17@live.com>
2023-06-05 22:09:24 -0700

    added delete button

--- waitingList.js
+++ waitingList.js
@@ -4,5 +4,25 @@ document.getElementById('add').addEventListener('click', () => {
     let table = document.getElementById('list');
     let row = table.insertRow(1);
     row.setAttribute('id', `item-${id}`);
-    row.insertCell(0).innerHTML = document.getElementById('customer-name').value
+    row.insertCell(0).innerHTML = document.getElementById('customer-name').value;
+    row.insertCell(1).innerHTML = document.getElementById('party-size').value;
+    row.insertCell(2).innerHTML = document.getElementById('phone-number').value;
+    let actions = row.insertCell(3);
+    actions.appendChild(createDeleteButton(id++));
+    document.getElementById('customer-name').value = '';
+    document.getElementById('party-size').value = '';
+    document.getElementById('phone-number').value = '';
 })
+
+function createDeleteButton(id) {
+    let btn = document.createElement('button');
+    btn.className = 'btn btn-primary';
+    btn.id = id;
+    btn.innerHTML = 'Delete';
+    btn.onclick = () => {
+        console.log(`Deleting row with id: item-${id}`);
+        let elementToDelete = document.getElementById(`item-${id}`);
+        elementToDelete.parentNode.removeChild(elementToDelete);
+    }
+    return btn
+}
